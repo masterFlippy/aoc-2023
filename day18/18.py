c6f2dadc0cd003807cbfec134ce14071c4a0e7b4
jonathan rengius <jonathan.rengius@sigmatechnology.com>
2023-12-19 15:02:09 +0100

    part2 day 18

--- day18/18.py
+++ day18/18.py
@@ -7,25 +7,56 @@ lines =  [
     }
     for line in input
 ]
-dir = {"R":(0,1), "D":(1,0), "L":(0,-1), "U":(-1,0)}
-instructions = []
-horizontal, vertical, around, inside = 0, 0, 0, 0
 
-for lineIndex,line in enumerate(lines):
-    instructions.append((line['direction'][0], int(line['direction'][1:])))
 
-for instruction in instructions:
-    direction = instruction[0]
-    steps = instruction[1]
-    directionVer = dir[direction][0]
-    directionHor = dir[direction][1]
-    directionVer *= steps
-    directionHor *= steps
-    vertical += directionVer
-    horizontal += directionHor
-    around += steps
-    inside += horizontal * directionVer
+def calculateInstructions(instructions):
+    dir = {"R":(0,1), "D":(1,0), "L":(0,-1), "U":(-1,0)}
+    horizontal, vertical, around, inside = 0, 0, 0, 0
 
-print(inside+around/2 + 1)
+    for instruction in instructions:
+        direction = instruction[0]
+        steps = instruction[1]
+        directionVer = dir[direction][0]
+        directionHor = dir[direction][1]
+        directionVer *= steps
+        directionHor *= steps
+        vertical += directionVer
+        horizontal += directionHor
+        around += steps
+        inside += horizontal * directionVer
+
+    return inside+around/2 + 1
+
+
+def getColorInstructions(lines):
+    directionDict = {
+        0:'R',
+        1:'D',
+        2:'L',
+        3:'U'
+    }
+    instructions = []
+    for line in lines:
+        hex = line["color"][1:]
+        direction = directionDict[int(hex[5:])]
+        steps = int(hex[:5], 16)
+        instructions.append((direction, steps))
+    return instructions
+
+def solveOne(lines):
+    instructions = []
+    for line in lines:
+        instructions.append((line['direction'][0], int(line['direction'][1:])))
+    return calculateInstructions(instructions)
+
+def solveTwo(lines):
+    colorInstructions = getColorInstructions(lines)
+    return calculateInstructions(colorInstructions)
+
+print(solveOne(lines))
+print(solveTwo(lines))
+
+
+
 
 
